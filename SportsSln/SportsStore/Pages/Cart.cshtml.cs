b2ab2e94e4803d2b2a73be8a72b37944db07ef99
heyzuess <heyzuess@users.noreply.github.com>
2020-11-04 21:48:31 -0800

    SportsStore - Shopping Cart WIP 1.2

--- SportsSln/SportsStore/Pages/Cart.cshtml.cs
+++ SportsSln/SportsStore/Pages/Cart.cshtml.cs
@@ -10,9 +10,10 @@ namespace SportsStore.Pages
     {
         private IStoreRepository repository;
 
-        public CartModel(IStoreRepository repo)
+        public CartModel(IStoreRepository repo, Cart cartService)
         {
             repository = repo;
+            Cart = cartService;
         }
 
         public Cart Cart { get; set; }
@@ -21,7 +22,6 @@ namespace SportsStore.Pages
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl ?? "/";
-            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
         }
 
         public IActionResult OnPost(long productId, string returnUrl)
@@ -29,9 +29,14 @@ namespace SportsStore.Pages
             Product product = repository.Products
                 .FirstOrDefault(p => p.ProductID == productId);
 
-            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
             Cart.AddItem(product, 1);
-            HttpContext.Session.SetJson("cart", Cart);
+            return RedirectToPage(new { returnUrl = returnUrl });
+        }
+
+        public IActionResult OnPostRemove(long productId, string returnUrl)
+        {
+            Cart.RemoveLine(Cart.Lines.First(cl =>
+                cl.Product.ProductID == productId).Product);
             return RedirectToPage(new { returnUrl = returnUrl });
         }
     }
